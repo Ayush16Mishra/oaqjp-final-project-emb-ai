0b8385b226b768f7957a04ca48a3ce6f33e31c12
Ayush16Mishra <Ayushpc025@gmail.com>
2026-05-29 10:31:56 -0400

    Complete AI emotion detection final project

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -24,7 +24,15 @@ def emotion_detector(text_to_analyze):
         json=input_json,
         headers=headers
     )
-
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
     formatted_response = json.loads(response.text)
 
     emotion_scores = formatted_response["emotionPredictions"][0]["emotion"]
